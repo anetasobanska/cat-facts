76d2d0d8d1c62eafb9b2f889a975d69c85fade50
anetasobanska <sannettka@gmail.com>
2022-11-21 15:44:16 +0100

    refactoring: deleted newCatFact functions

--- script.js
+++ script.js
@@ -3,25 +3,17 @@ const catFactText = document.getElementById('fact');
 const twitterBtn = document.getElementById('twitter');
 const newCatFactBtn = document.getElementById('new-fact');
 
-let dataCat = '';
-
-function newCatFact() {
-    const catFact = dataCat;
-    if(catFact.fact.length > 100) {
-        catFactText.classList.add('fact-text-long')
-    } else {
-        catFactText.classList.remove('fact-text-long')
-    }
-    catFactText.textContent = catFact.fact;
-}
-
 async function fetchCatFact() {
     const apiUrl = 'https://catfact.ninja/fact';
         try {
             const res = await fetch(apiUrl);
             dataCat = await res.json();
-            console.log(dataCat);
-            newCatFact();
+            if(dataCat.fact.length > 100) {
+                catFactText.classList.add('fact-text-long')
+            } else {
+                catFactText.classList.remove('fact-text-long')
+            }
+            catFactText.textContent = dataCat.fact;
         } catch (e) {
             console.log(e)
         }
@@ -31,5 +23,8 @@ function tweetCatFact() {
     const twitterUrl = `https://twitter.com/intent/tweet?text=${catFactText.textContent}`;
     window.open(twitterUrl, '_blank');
 }
+
+newCatFactBtn.addEventListener('click', fetchCatFact);
+twitterBtn.addEventListener('click', tweetCatFact);
 
 fetchCatFact();
